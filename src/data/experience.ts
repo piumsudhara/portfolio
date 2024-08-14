f4638426ade6308337eb70a69b00a144147e343a
Pium Sudhara <piumsudhara@Piums-Laptop.local>
2024-08-14 15:38:06 +0530

    Upgrade portfolio #1

---
+++ src/data/experience.ts
@@ -0,0 +1,54 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  // {
+  //   dates: "August 2024 — Present",
+  //   title: "Senior Cloud Operations Engineer",
+  //   company: "London Stock Exchange Group",
+  //   companyUrl: "https://www.lseg.com/en",
+  //   description:
+  //     "LSEG is one of the world’s leading providers of financial markets infrastructure and delivers financial data, analytics, news and index products to more than 40,000 customers in over 170 countries.",
+  //     technologies: ["AWS", "GCP", "Azure", "Terraform", "Vault", "Ansible", "Docker", "Kubenertes", "Jenkins", "GitLab", "DataDog", "Consul", "Python"],
+  //   logo: "/logos/lseg.png",
+  // },
+  {
+    dates: "February 2022 — Present",
+    title: "Cloud Operations Engineer",
+    company: "London Stock Exchange Group",
+    companyUrl: "https://www.lseg.com/en",
+    description:
+      "LSEG is one of the world’s leading providers of financial markets infrastructure and delivers financial data, analytics, news and index products to more than 40,000 customers in over 170 countries.",
+      technologies: ["AWS", "GCP", "Azure", "Terraform", "Vault", "Ansible", "Docker", "Kubenertes", "Jenkins", "GitLab", "DataDog", "Consul", "Python"],
+    logo: "/logos/lseg.png",
+  },
+  {
+    dates: "October 2021 — February 2022",
+    title: "Associate Site Reliability Engineer",
+    company: "Chakray Consultancy",
+    companyUrl: "https://www.chakray.com/",
+    description:
+      "Design strategies and implement best of breed Integration, API, DevOps and Identity technologies",
+      technologies: ["WSO2", "AWS", "Kubernetes", "Terraform", "Docker", "Jenkins", "Grafana", "Promethus", "EFK"],
+    logo: "/logos/chakray.jpg",
+  },
+  {
+    dates: "November 2019 — October 2021",
+    title: "Associate DevOps Engineer",
+    company: "CodeZilla",
+    companyUrl: "https://codezilla.lk/",
+    description:
+      "Outsourcing tech solutions and growing the diversity of services Codezilla is a full-stack tech solutions agency with all the specialists required for an efficient creative process from scratch.",
+    technologies: ["AWS", "GCP", "Terraform", "Firebase", "Heroku", "Netlify", "PHP", "React", "Node", "Kubernetes", "Docker", "GitHub Actions"],
+    logo: "/logos/codezilla.png",
+  },
+  {
+    dates: "August 2019 — October 2019",
+    title: "Associate Software Engineer",
+    company: "Moneta",
+    companyUrl: "https://www.moneta.lk/",
+    description:
+      "Moneta is the official brand of FinTech Hive (PVT) Ltd., a software service provider development and maintenance of 'Moneta' the financial Android Application.",
+    technologies: ["Android", "Firebase", "GCP"],
+    logo: "/logos/moneta.png",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,27 @@
+import IconMedium from "../components/icons/medium.astro";
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/piumsudhara",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/pium-sudhara-karunasena-41554458/",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Twitter",
+    url: "https://x.com/piumsudhara",
+    icon: IconTwitter,
+  },
+  {
+    name: "Medium",
+    url: "https://medium.com/@piumsudhara",
+    icon: IconMedium,
+  },
+];
