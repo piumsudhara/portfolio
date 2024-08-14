b9864ff34d8757d5bf67eaa6cbbfd3d0ed58ee5d
Pium Sudhara <pium.karunasena@gmail.com>
2024-08-14 18:11:46 +0530

    Update links

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -5,23 +5,23 @@ import IconTwitter from "../components/icons/twitter.astro";
 
 export const SOCIALS = [
   {
-    name: "Github",
-    url: "https://github.com/piumsudhara",
-    icon: IconGithub,
-  },
-  {
     name: "Linkedin",
     url: "https://www.linkedin.com/in/pium-sudhara-karunasena-41554458/",
     icon: IconLinkedin,
   },
   {
-    name: "Twitter",
-    url: "https://x.com/piumsudhara",
-    icon: IconTwitter,
+    name: "Github",
+    url: "https://github.com/piumsudhara",
+    icon: IconGithub,
   },
   {
     name: "Medium",
     url: "https://medium.com/@piumsudhara",
     icon: IconMedium,
+  },
+  {
+    name: "Twitter",
+    url: "https://x.com/piumsudhara",
+    icon: IconTwitter,
   },
 ];
