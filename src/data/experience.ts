61d0a35fdbda0e4c5ab7e765552bdfaf5b48adbb
Pium Sudhara <piumsudhara@Piums-MacBook-Air.local>
2026-06-03 22:40:56 +0530

    Update current job date

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,14 +1,24 @@
 import type { Props as ExperienceProp } from "../components/experience/props";
 
 export const EXPERIENCE: ExperienceProp[] = [
+  // {
+  //   dates: "July 2026 — Present",
+  //   title: "Associate Technical Lead - DevOps",
+  //   company: "LSEG",
+  //   companyUrl: "https://www.lseg.com/en",
+  //   description:
+  //     "LSEG is one of the world’s leading providers of financial markets infrastructure and delivers financial data, analytics, news and index products to more than 40,000 customers in over 170 countries.",
+  //     technologies: ["Azure"],
+  //   logo: "/logos/lseg.png",
+  // },
   {
-    dates: "March 2025 — Present",
+    dates: "March 2025 — July 2026",
     title: "Senior DevOps Engineer",
     company: "IFS",
     companyUrl: "https://www.ifs.com",
     description:
       "IFS is the world’s leading provider of industrial AI and enterprise software for hardcore businesses that service, power and protect our planet.",
-      technologies: ["Azure"],
+      technologies: ["Azure", "Terraform", "Docker", "Kubenertes", "Apache Kafka", "GitHub Actions", "Python", "Prometheus", "Grafana"],
     logo: "/logos/ifs.jpg",
   },
   {
@@ -42,7 +52,7 @@ export const EXPERIENCE: ExperienceProp[] = [
     logo: "/logos/chakray.jpg",
   },
   {
-    dates: "November 2019 — October 2021",
+    dates: "August 2019 — October 2021",
     title: "Associate DevOps Engineer",
     company: "CodeZilla",
     companyUrl: "https://codezilla.lk/",
@@ -50,15 +60,5 @@ export const EXPERIENCE: ExperienceProp[] = [
       "Outsourcing tech solutions and growing the diversity of services Codezilla is a full-stack tech solutions agency with all the specialists required for an efficient creative process from scratch.",
     technologies: ["AWS", "GCP", "Terraform", "Firebase", "Heroku", "Netlify", "PHP", "React", "Node", "Kubernetes", "Docker", "GitHub Actions"],
     logo: "/logos/codezilla.png",
-  },
-  {
-    dates: "August 2019 — October 2019",
-    title: "Associate Software Engineer",
-    company: "Moneta",
-    companyUrl: "https://www.moneta.lk/",
-    description:
-      "Moneta is the official brand of FinTech Hive (PVT) Ltd., a software service provider development and maintenance of 'Moneta' the financial Android Application.",
-    technologies: ["Android", "Firebase", "GCP"],
-    logo: "/logos/moneta.png",
   },
 ];
